97dd56c2f1d5a0a9ff61d88814546622fe5887d5
Suspeakan <130405428+Suspeakan@users.noreply.github.com>
2023-09-10 18:40:36 -0300

    Add files via upload

--- spdztranslitapp.py
+++ spdztranslitapp.py
@@ -12,3 +12,6 @@ def index():
         result = spdzonlytranslit.convert_to_cyrillic2(input_text)
 
     return render_template('index.html', result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
